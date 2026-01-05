aaa8848b3d8489f3eaaee590e6c200c92bce0e9e
Giovanni Baratta <bargiovi@hotmail.it>
2026-01-05 18:48:18 +0100

    chore: rename approve client file

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@
 export * from "./interfaces"
 export * from "./auth/user.authenticator"
 export * from "./auth/auth.helpers"
-export * from "./client/approvio.client"
+export * from "./client/user.client"
 export * from "./client/base.client"
